5905e07774b17e669fe79ec4de3a14a17b704796
Luca Schirmer <acu1410l@gmail.com>
2023-05-27 03:26:57 +0200

    --

--- flutter-setup-support/extension.js
+++ flutter-setup-support/extension.js
@@ -1,9 +1,6 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 const vscode = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -11,15 +8,13 @@ const vscode = require('vscode');
 // Executed only once at the start
 function activate(context) {
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('flutter-setup-support.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from flutter setup support!');
-	});
+	let disposable = vscode.commands.registerCommand('flutter-setup-support.createAppNavigationHandler',  
+		() => {
+
+			vscode.window.showInformationMessage('Hello World from flutter setup support!');
+		}
+	);
 
 	context.subscriptions.push(disposable);
 }
