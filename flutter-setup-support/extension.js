55aa9bd049ff36b1a776e80bdb51a64bcb568aaf
Luca Schirmer <acu1410l@gmail.com>
2023-05-28 22:25:38 +0200

    Update extension.js

--- flutter-setup-support/extension.js
+++ flutter-setup-support/extension.js
@@ -1,6 +1,7 @@
 
 const vscode = require('vscode');
-
+const fs = require('fs')
+const fileUrl = vscode.Uri.file('text.txt');
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -8,10 +9,16 @@ const vscode = require('vscode');
 // Executed only once at the start
 function activate(context) {
 
-
+	
 	let disposable = vscode.commands.registerCommand('flutter-setup-support.createAppNavigationHandler',  
 		() => {
-
+			vscode.window.showInformationMessage('fs');
+			try {
+				fs.writeFile(fileUrl, "based");
+			} catch (error) { 
+				console.error(error)
+			}
+			
 			vscode.window.showInformationMessage('Hello World from flutter setup support!');
 		}
 	);
